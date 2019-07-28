727146ffa27ada6c52951291e00a1c95c08af30b
Topgyal Gurung <topgyaltsering3@gmail.com>
2019-07-27 20:14:35 -0400

    modify connect

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,22 +1,13 @@
 //combines all models and export them as database interface 
 //to Express application
 
-const mongoose =require('mongoose');
-DATABASE_URL='mongodb://localhost:27017/myapp'
+var mongoose =require('mongoose');
+
 
 const User =require('./user.js');
 const Message =require('./message.js');
 
-// mongoose.connect(DATABASE_URL,{useNewUrlParser: true});
-
-// var connectDb=mongoose.connection;
-// connectDb.on('error', function(err){
-//     console.log(error("Mongoose default connection has occured "+err+" error"));
-// });
-    
-//const connectDb=mongoose.connection;
-
 const models={User,Message};
 
-module.exports= {connectDb};
+// module.exports= {connectDb};
 module.exports=  models;
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,6 +2,14 @@ import session from '../session';
 import user from './user';
 import message from './message';
 
+const router=require("express").Router();
+
+router.get('/',function(req,res,next){
+    res.send('index',{title:'Express'});
+})
+
 export default {
     session,user,message
 };
+
+module.exports =router;
